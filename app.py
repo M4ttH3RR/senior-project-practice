973c59622e5abbed0b44af9e59057599fcf8abc5
Matthew Herriman <165517184+M4ttH3RR@users.noreply.github.com>
2026-09-08 13:40:19 -0400

    Add student developer profile

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-Matthew Herriman 
-Computer Science
-Cybersecurity
-Networking fundamentals
+print("Senior Project Developer Profile")
+print()
+print("Name: Matthew Herriman")
+print("Major: Computer Science")
+print("Technology Interest: Cybersecurity")
+print("Skill Goal: Software Development")
